116e313a7854e67c77f52db2d27efb39f2316498
Douša Marek <346-dousamare@users.noreply.localhost>
2021-08-25 12:27:14 +0000

    pomocna globalni trida se vsemi daty nahranymi z menu

---
+++ Menu/SessionData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Stitcher360
+{
+    public class SessionData
+    {
+        public Image[] LoadedImages { get; set; }
+        public int OutResolutionX { get; set; }
+        public int OutResolutionY { get; set; }
+        public int ImageResolutionX { get; set; }
+        public int ImageResolutionY { get; set; }
+        public int NumberOfRows { get; set; }
+        public int NumberOfCols { get; set; }
+    }
+}
