195581a670ad1844acea4fe5e324ac37125695b3
Douša Marek <346-dousamare@users.noreply.localhost>
2021-08-25 12:26:43 +0000

    pomocna trida s coords

---
+++ Menu/SphereCoords.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stitcher360
+{
+    class SphereCoords
+    {
+        public SphereCoords(double lat, double lon)
+        {
+            this.lat = lat;
+            this.lon = lon;
+        }
+
+        public double lat { get; set; }
+        public double lon { get; set; }
+    }
+}
