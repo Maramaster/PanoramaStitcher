b73e6637ac72c2cd1a60dbb30f63166547e11222
Douša Marek <346-dousamare@users.noreply.localhost>
2021-08-25 12:25:15 +0000

    stitch button, ulozeni fotek do session data

--- Menu/Form1.cs
+++ Menu/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,39 +12,42 @@ using System.Windows.Forms;
 namespace Stitcher360
 {
     public partial class Form1 : Form
-    { 
+    {
         //TODO: fullscreen + redo design 2:1 nahore na upload a dolni cast vlevo fl, rows a cols, dolni cast vpravo advanced
-		public Form1()
+        SessionData sessionData = new SessionData();
+        public Form1()
         {
             InitializeComponent();
             InitializeMenu();
         }
 
         public void InitializeMenu()
-		{
-			int widthOfButton = 100;
-			int heightOfButton = 70;
+        {
+            int widthOfButton = 100;
+            int heightOfButton = 70;
 
-			//location X, location Y, name of a button, text inside button, width and height of button
-			CreateButton(widthOfButton, this.Bottom/2, "button1", "Load images", widthOfButton, heightOfButton);
+            //location X, location Y, name of a button, text inside button, width and height of button
+            CreateButton(widthOfButton, this.Bottom / 2, "button1", "Load images", widthOfButton, heightOfButton);
             CreateButton(this.Width - widthOfButton, this.Top, "button2", "Advanced", widthOfButton, heightOfButton);
-			CreateButton(this.Width - widthOfButton, (this.Bottom / 3)*2, "button3", "Repaire Vignetattion", widthOfButton, heightOfButton);
-			CreateButton(this.Width - widthOfButton, this.Bottom / 3, "button4", "Repaire Lens Distortion", widthOfButton, heightOfButton);
+            CreateButton(this.Width - widthOfButton, (this.Bottom / 3) * 2, "button3", "Remove Vignette", widthOfButton, heightOfButton);
+            CreateButton(this.Width - widthOfButton, this.Bottom / 3, "button4", "Repair Lens Distortion", widthOfButton, heightOfButton);
+            CreateButton(widthOfButton, 2 * this.Bottom / 3, "button5", "Stitch Images", widthOfButton, heightOfButton);
 
 
             int widthOfTextbox = 200;
             int heightOfTextbox = 40;
             //location X, location Y, name of a textbox, text inside textbox, width and height of textbox
-            CreateTextbox(this.Width / 2, (this.Bottom / 4)*1, "textBox1", "Input Focal Lenght", widthOfTextbox, heightOfTextbox);
-            CreateTextbox(this.Width / 2, (this.Bottom / 4)*2, "textBox2", "Input Number of Pictures in Row", widthOfTextbox, heightOfTextbox);
-            CreateTextbox(this.Width / 2, (this.Bottom / 4)*3, "textBox3", "Input Number of Pictures in Collumn", widthOfTextbox, heightOfTextbox);
+            CreateTextbox(this.Width / 2, (this.Bottom / 4) * 1, "textBox1", "Input Focal Lenght", widthOfTextbox, heightOfTextbox);
+            CreateTextbox(this.Width / 2, (this.Bottom / 4) * 2, "textBox2", "Input Number of Pictures in Row", widthOfTextbox, heightOfTextbox);
+            CreateTextbox(this.Width / 2, (this.Bottom / 4) * 3, "textBox3", "Input Number of Pictures in Collumn", widthOfTextbox, heightOfTextbox);
 
+            //TODO: pridat permanentni textovy popisky nad textovy okna, protoze kdyz uz jednou zmizej tak se znova neobjevi
             //TODO: rows a cols
             //TODO: adaptivne dopocitat rows?
         }
 
-		private void CreateTextbox(int X, int Y, string name, string text, int width, int height)
-		{
+        private void CreateTextbox(int X, int Y, string name, string text, int width, int height)
+        {
             // Create and initialize a TextBox.
             TextBox textBox = new TextBox();
 
@@ -59,20 +63,18 @@ namespace Stitcher360
             textBox.Click += new System.EventHandler(this.ClearTextBox);
         }
 
+        public void CreateButton(int X, int Y, string name, string text, int width, int height)
+        {
+            // Create and initialize a Button.
+            Button button = new Button();
 
 
-		public void CreateButton(int X, int Y, string name, string text, int width, int height)
-		{
-			// Create and initialize a Button.
-			Button button = new Button();
-
-
-			button.Location = new System.Drawing.Point(X, Y);
-			button.Name = name;
-			button.Size = new System.Drawing.Size(width, height);
-			button.TabIndex = 4;
-			button.Text = text;
-			button.UseVisualStyleBackColor = true;
+            button.Location = new System.Drawing.Point(X, Y);
+            button.Name = name;
+            button.Size = new System.Drawing.Size(width, height);
+            button.TabIndex = 4;
+            button.Text = text;
+            button.UseVisualStyleBackColor = true;
 
 
             // TODO: delegatove
@@ -86,16 +88,19 @@ namespace Stitcher360
                     button.Click += new System.EventHandler(this.Advanced_Click);
                     break;
 
-                case "Repaire Vignetattion":
+                case "Remove Vignette":
                     button.Click += new System.EventHandler(this.RepaireVignetattion_Click);
                     button.Visible = false;
                     button.Hide();
                     break;
 
-                case "Repaire Lens Distortion":
+                case "Repair Lens Distortion":
                     button.Click += new System.EventHandler(this.RepaireLens_Click);
                     button.Visible = false;
                     button.Hide();
+                    break;
+                case "Stitch Images":
+                    button.Click += new System.EventHandler(this.StitchImages_Click);
                     break;
 
                 default:
@@ -103,45 +108,56 @@ namespace Stitcher360
                     break;
             }
 
-
             // Add the button to the form.
             Controls.Add(button);
-		}
+        }
 
-		private void RepaireLens_Click(object sender, EventArgs e)
-		{
-			throw new NotImplementedException();
-		}
+        private void StitchImages_Click(object sender, EventArgs e)
+        {
+            //TODO: kontrola jestli tam jsou obrazky v session datech
+            Bitmap finalresult = new Bitmap(sessionData.OutResolutionX, sessionData.OutResolutionY);
+            finalresult = PhotoAssembler.StitchPhotos(sessionData);
+            SaveFileDialog dialog = new SaveFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                finalresult.Save(dialog.FileName, ImageFormat.Jpeg);
+            }
+        }
 
-		private void RepaireVignetattion_Click(object sender, EventArgs e)
-		{
+        private void RepaireLens_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RepaireVignetattion_Click(object sender, EventArgs e)
+        {
             //TODO: nejdriv se zeptat na nahrani masky na vignetaci a pak se to magicky opravi
 
-			throw new NotImplementedException();
-		}
+            throw new NotImplementedException();
+        }   
 
         //hides extra tools
-		private void Advanced_Click(object sender, EventArgs e)
-		{
-            //Controls[2] and Controls[3] are buttons Repaire Vignetattion and Repaire Lens Distortion
+        private void Advanced_Click(object sender, EventArgs e)
+        {
+            //Controls[2] and Controls[3] are buttons Remove Vignette and Repair Lens Distortion
             if (Controls[2].Visible)
-			{
+            {
                 Controls[2].Visible = false;
                 Controls[3].Visible = false;
                 Controls[2].Refresh();
                 Controls[3].Refresh();
 
             }
-			else
-			{
+            else
+            {
                 Controls[2].Visible = true;
                 Controls[3].Visible = true;
                 Controls[2].Refresh();
                 Controls[3].Refresh();
             }
-		}
+        }
 
-		private void LoadImage_Click(object sender, EventArgs e)
+        private void LoadImage_Click(object sender, EventArgs e)
         {
             using (OpenFileDialog openFileDialog = new OpenFileDialog()
             {
@@ -149,13 +165,20 @@ namespace Stitcher360
                 Filter = "Image File|*.jpg;*.png"
             })
             {
+                openFileDialog.Multiselect = true;
                 if (openFileDialog.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
                 {
                     //string selectedFile = openFileDialog.FileName;
 
                     //getting selected image
-                    
+                    sessionData.LoadedImages = new Image[openFileDialog.FileNames.Length];
                     Image image = Image.FromFile(openFileDialog.FileName);
+                    for (int i = 0; i < openFileDialog.FileNames.Length; i++)
+                    {
+                        sessionData.LoadedImages[i] = Image.FromFile(openFileDialog.FileNames[i]);
+                    }
+
+                    // TODO: for image in sessionData.LoadedImages do: image.display()
 
                     PictureBox picture = new PictureBox
                     {
@@ -178,13 +201,14 @@ namespace Stitcher360
         }
         private void ClearTextBox(object sender, EventArgs e)
         {
+            //TODO: smazat a dat text nad box
             //only on TextBoxes, therefore no error could accour
-		    ((TextBox)sender).Text = "";
+            ((TextBox)sender).Text = "";
         }
 
         private void Form1_Click(object sender, EventArgs e)
         {
-			//this.Close();
+            //this.Close();
         }
     }
 }
