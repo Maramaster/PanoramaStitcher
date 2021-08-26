76d5f23d37189a16e44969b2183184226987b27a
Douša Marek <346-dousamare@users.noreply.localhost>
2021-08-26 13:56:42 +0000

    nova class na validaci inputu popripade vsechno jineho

---
+++ Menu/Validator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stitcher360
+{
+	class Validator
+	{
+		public static bool isNumber(string testString)
+		{
+			return int.TryParse(testString, out _);
+		}
+	}
+}
