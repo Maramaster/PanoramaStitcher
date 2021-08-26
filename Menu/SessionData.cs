14e8872317c04a83d861f7df2220e6c672de1a94
Douša Marek <346-dousamare@users.noreply.localhost>
2021-08-26 13:55:47 +0000

    Replace SessionData.cs

--- Menu/SessionData.cs
+++ Menu/SessionData.cs
@@ -9,12 +9,13 @@ namespace Stitcher360
 {
     public class SessionData
     {
-        public Image[] LoadedImages { get; set; }
+        public Bitmap[] LoadedImages { get; set; }
         public int OutResolutionX { get; set; }
         public int OutResolutionY { get; set; }
         public int ImageResolutionX { get; set; }
         public int ImageResolutionY { get; set; }
-        public int NumberOfRows { get; set; }
-        public int NumberOfCols { get; set; }
+        public int NumberOfPicturesInRow { get; set; }
+        public int NumberOfPicturesInCol { get; set; }
+        public int FocalLenght { get; set; }
     }
 }
