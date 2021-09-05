847449649946e2a73aed3e96ac6479b933c1c934
Douša Marek <346-dousamare@users.noreply.localhost>
2021-09-05 09:32:23 +0000

    Replace Validator.cs

--- Menu/Validator.cs
+++ Menu/Validator.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace Stitcher360
 {
+	/// <summary>
+	/// This class is used to check whether sessionData is in a correct format before being sent to the main stitching function.
+	/// </summary>
 	class Validator
 	{
 		public static bool isNumber(string testString)
