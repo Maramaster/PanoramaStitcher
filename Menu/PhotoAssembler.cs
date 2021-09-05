b49c6f96f9a16b4882bef21ee93981460bfdfbba
Douša Marek <346-dousamare@users.noreply.localhost>
2021-09-05 09:30:32 +0000

    Replace PhotoAssembler.cs

--- Menu/PhotoAssembler.cs
+++ Menu/PhotoAssembler.cs
@@ -4,99 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.IO;
 
 namespace Stitcher360
 {
 	class PhotoAssembler
 	{
+		/// <summary>
+		/// Main function overseeing whole stiting process
+		/// </summary>
+		/// <param name="sessionData"></param>
+		/// <returns></returns>
 		public static Bitmap StitchPhotos(SessionData sessionData)
 		{
 
-            if (sessionData.OutResolutionX != sessionData.OutResolutionY * 2)
-            {
-                // An equirectangular projection requires 2x1 image format
-                throw new FormatException();
-            }
-            Bitmap output = new Bitmap(sessionData.OutResolutionX, sessionData.OutResolutionY);
-            SphereCoords[] photoCenters = GetPhotocenters(sessionData);
-
-            Color last = Color.Black;
-            for (int x = 0; x < sessionData.OutResolutionX; x++)
-            {
-                for (int y = 0; y < sessionData.OutResolutionY; y++)
-                {
-                    output.SetPixel(x, y, GetPixelFromSphere(x, y, sessionData, photoCenters));
-                }
-            }
-            return output;
-        }
-
-		//For each picture one center
-		private static SphereCoords[] GetPhotocenters(SessionData sessionData)
-		{
-			//TODO: automaticky dopocitat heightSeparator
-
-			//pouze pro 10x2
-			int heightSeparator = 80;
-			SphereCoords[] output = new SphereCoords[sessionData.LoadedImages.Length];
-			double[] heightCenters = getHeightCenters(sessionData.NumberOfPicturesInCol);
-			for (int i = 0; i < sessionData.NumberOfPicturesInCol; i++)
+			if (sessionData.OutResolutionX != sessionData.OutResolutionY * 2)
 			{
-				for (int j = 0; j < sessionData.NumberOfPicturesInRow; j++)
+				// An equirectangular projection requires 2x1 image format
+				throw new FormatException();
+			}
+			Bitmap output = new Bitmap(sessionData.OutResolutionX, sessionData.OutResolutionY);
+			SphereVec[] photoCenters = PhotoCenters.GetPhotocenters(sessionData);
+
+			for (int x = 0; x < sessionData.OutResolutionX; x++)
+			{
+				for (int y = 0; y < sessionData.OutResolutionY; y++)
 				{
-					output[i * sessionData.NumberOfPicturesInRow + j] = new SphereCoords(360 / sessionData.NumberOfPicturesInRow * j, heightCenters[i] * heightSeparator);
+					output.SetPixel(x, y, GetPixelFromSphere(x, y, sessionData, photoCenters));
 				}
 			}
 			return output;
 		}
 
-		private static double[] getHeightCenters(int numberOfPicturesInCol)
-		{
-			double[] centers = new double[numberOfPicturesInCol];
-			if (numberOfPicturesInCol % 2 == 0)
-			{
-				int[] arr = Enumerable.Range(0, numberOfPicturesInCol / 2).ToArray();
-				int[] firstPart = Enumerable.Reverse(arr).ToArray();
-				int[] secondPart = Enumerable.Range(0, numberOfPicturesInCol / 2).ToArray();
-
-				for (int i = 0; i < numberOfPicturesInCol; i++)
-				{
-					if (i < numberOfPicturesInCol / 2) { centers[i] = (double)firstPart[i] + (0.5); }
-					else { centers[i] = -1 * ((double)secondPart[i - numberOfPicturesInCol / 2] + (0.5)); }
-				}
-			}
-			else
-			{
-				int[] arr = Enumerable.Range(1, (numberOfPicturesInCol / 2)).ToArray();
-				int[] firstPart = Enumerable.Reverse(arr).ToArray();
-				int[] secondPart = Enumerable.Range(1, (numberOfPicturesInCol / 2)).ToArray();
-
-				for (int i = 0; i < numberOfPicturesInCol; i++)
-				{
-					if (i < numberOfPicturesInCol / 2) { centers[i] = (double)firstPart[i]; }
-					else if (i == numberOfPicturesInCol / 2) { centers[i] = 0; }
-					else { centers[i] = -1 * ((double)secondPart[i - 1 - numberOfPicturesInCol / 2]); }
-				}
-			}
-			return centers;
-		}
-
-		private static Color GetPixelFromSphere(int x, int y, SessionData sessionData, SphereCoords[] photoCenters)
+		private static Color GetPixelFromSphere(int x, int y, SessionData sessionData, SphereVec[] photoCenters)
 		{
 			// We need to modify the x coord slightly to get the correct degree coordinate system
 			x -= sessionData.OutResolutionX / photoCenters.Length / 2;
-			SphereCoords currentRay = new SphereCoords(x, y, sessionData.OutResolutionX, sessionData.OutResolutionY);
+			SphereVec currentRay = new SphereVec();
+			currentRay.getPointFromCylinder(x, y, sessionData.OutResolutionX, sessionData.OutResolutionY);
 
 			// Figure out which photo is closest to the current view ray
 			int selectedImageSegment = GetClosestImgCoord(photoCenters, currentRay);
 
-            // Get the exact pixel from the photo we are looking at           
-            double scaleY = 450;
-            double scaleX = scaleY;
-            //double scaleX = (scaleY / Math.PI) / 2;
-            Color colorFromInput = GetColorFromInput(photoCenters[selectedImageSegment], sessionData.LoadedImages[selectedImageSegment], currentRay, scaleX, scaleY);
-            return colorFromInput;
-        }
+			// Get the exact pixel from the photo we are looking at           
+			double scaleY = 300;
+			double scaleX = scaleY;
+			Color colorFromInput = GetColorFromInput(photoCenters[selectedImageSegment], sessionData.LoadedImages[selectedImageSegment], currentRay, scaleX, scaleY);
+			return colorFromInput;
+		}
 
 		/// <summary>
 		/// Handles accessing the correct pixel from the input data 
@@ -107,30 +62,55 @@ namespace Stitcher360
 		/// <param name="scaleX"></param>
 		/// <param name="scaleY"></param>
 		/// <returns></returns>
-		private static Color GetColorFromInput(SphereCoords sphereCoords, Bitmap image, SphereCoords currentRay, double scaleX, double scaleY)
+		private static Color GetColorFromInput(SphereVec photoCenter, Bitmap image, SphereVec currentRay, double scaleX, double scaleY)
 		{
-			double deltaLat = currentRay.lat - sphereCoords.lat;
-			double deltaLon = currentRay.lon - sphereCoords.lon;
+			int[] pointOnPlane = GetPointOnPicture(photoCenter,currentRay);
 
-            int x = (int)((Math.Tan(SphereCoords.ToRadians(deltaLat)) * scaleX) + image.Width / 2);
-            int y = image.Height - (int)((Math.Tan(SphereCoords.ToRadians(deltaLon)) * scaleY) + image.Height / 2);
-            if (x >= 0 && x < image.Width && y >= 0 && y < image.Height)
-            {
-                return image.GetPixel(x, y);
-            }
-            else
-            {
-                return Color.Black;
-            }
+			int x = pointOnPlane[0];
+			int y = pointOnPlane[1];
+			if (x >= 0 && x < image.Width * 2 && y >= 0 && y < image.Height * 2)
+			{
+				return image.GetPixel(x, y);
+			}
+			else
+			{
+				return Color.Black;
+			}
+		}
+
+        private static int[] GetPointOnPicture(SphereVec photoCenter, SphereVec currentRay)
+        {
+			// We are finding out where will the ray intersect the picture, which translates to
+			// the intersection of a line and a plane
+			int[] points = new int[2];
+
+			int a = (int)photoCenter.X;
+			int b = (int)photoCenter.Y;
+			int c = (int)photoCenter.Z;
+			int d = (int)(Math.Pow(a,2) + Math.Pow(b, 2) + Math.Pow(c, 2));
+
+			int RayX = (int)currentRay.X;
+			int RayY = (int)currentRay.Y;
+			int RayZ = (int)currentRay.Z;
+
+			// Solve for t
+
+			double t = -d / ((a * RayX) + (b * RayY) + (c * RayZ));
+
+			// This is the absolute position of where currentRay (our rendering ray) intersects the plane of the photo
+			Vector absolutePosition = new Vector(t*RayX,t*RayY,t*RayZ);
+			PointDebugger.WritePointToTxt(new SphereVec(absolutePosition.X, absolutePosition.Y, absolutePosition.Z), "Polyhedron.txt");
+			return points;
         }
 
-		/// <summary>
-		/// For the current view requested by the render answer which picture is closest to this angle to be accessed.
-		/// </summary>
-		/// <param name="photoCenters">Array of centers of all photographs</param>
-		/// <param name="currentRay">The current view</param>
-		/// <returns></returns>
-		private static int GetClosestImgCoord(SphereCoords[] photoCenters, SphereCoords currentRay)
+
+        /// <summary>
+        /// For the current view requested by the render answer which picture is closest to this angle to be accessed.
+        /// </summary>
+        /// <param name="photoCenters">Array of centers of all photographs</param>
+        /// <param name="currentRay">The current view</param>
+        /// <returns></returns>
+        private static int GetClosestImgCoord(SphereVec[] photoCenters, SphereVec currentRay)
 		{
 			int minI = 0;
 			double minDistance = double.MaxValue;
