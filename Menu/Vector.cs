2987d1a3a59f9ec8f6928d0002410a65b539942f
Douša Marek <346-dousamare@users.noreply.localhost>
2021-09-05 09:32:37 +0000

    Upload New File

---
+++ Menu/Vector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stitcher360
+{
+	class Vector
+	{
+		public double X { get; set; }
+		public double Y { get; set; }
+		public double Z { get; set; }
+		public Vector(double x, double y, double z)
+		{
+			this.X = x;
+			this.Y = y;
+			this.Z = z;
+		}
+	}
+}
