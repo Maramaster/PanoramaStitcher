73785460d40f818b3bf079131ab84d484e7e84b0
Douša Marek <346-dousamare@users.noreply.localhost>
2021-09-09 14:01:07 +0000

    final version

--- Menu/PhotoCenters.cs
+++ Menu/PhotoCenters.cs
@@ -6,89 +6,51 @@ using System.Threading.Tasks;
 
 namespace Stitcher360
 {
-	/// <summary>
-	/// Generates centers of all photos.
-	/// </summary>
-    class PhotoCenters
+    class PhotoCenter
     {
-		public static SphereVec[] GetPhotocenters(SessionData sessionData)
+		public double X { get; set; }
+		public double Y { get; set; }
+		public double Z { get; set; }
+        public double lon { get; set; }
+        public double lat { get; set; }
+        public PhotoCenter(double x, double y, double z, double lon, double lat)
 		{
-			//TODO: automaticky dopocitat angle
-			int angle = 40;
-			//pozor *2 protoze to vychazi na rozdil 40
-			int heightSeparatorAngle = (int)(Math.Sin(SphereVec.ToRadians(angle))*sessionData.Radius)*2;
-			
-			SphereVec[] output = new SphereVec[sessionData.LoadedImages.Length];
-			
-			double[] heightCenters = getHeightCenters(sessionData.NumberOfPicturesInCol);
-			double[] arrayXandY;
-			for (int i = 0; i < sessionData.NumberOfPicturesInCol; i++)
-			{
-				for (int j = 0; j < sessionData.NumberOfPicturesInRow; j++)
-				{
-					arrayXandY = getXandY(j, sessionData, Math.Asin((heightCenters[i] * heightSeparatorAngle)/sessionData.Radius));
-					output[i * sessionData.NumberOfPicturesInRow + j] = new SphereVec(arrayXandY[0], arrayXandY[1] ,heightCenters[i] * heightSeparatorAngle);
-				}
-			}
-			return output;
+			this.X = x;
+			this.Y = y;
+			this.Z = z;
+			this.lon = lon;
+			this.lat = lat;
+		}
+
+		//euklidean metric distance
+		public double DistanceFrom(SphereVec other)
+		{
+			return Math.Sqrt(
+				Math.Pow((double)(this.X - other.X), 2)
+				+ Math.Pow((double)(this.Y - other.Y), 2)
+				+ Math.Pow((double)(this.Z - other.Z), 2)
+			);
 		}
 
 		/// <summary>
-		/// gets coordinates of an X and Y point from circle
+		/// For the current view requested by the render answer which picture is closest to this angle to be accessed.
 		/// </summary>
-		/// <param name="i"></param>
-		/// <param name="sessionData"></param>
-		/// <param name="heightSeparatorAngle"></param>
+		/// <param name="photoCenters">Array of centers of all photographs</param>
+		/// <param name="currentRay">The current view</param>
 		/// <returns></returns>
-		private static double[] getXandY(int i, SessionData sessionData, double heightSeparatorAngle)
+		public static int GetClosestImgCoord(PhotoCenter[] photoCenters, SphereVec currentRay)
 		{
-			
-			double[] array = new double[2];
-			int totalPoints = sessionData.NumberOfPicturesInRow;
-
-			var theta = ((Math.PI * 2) / totalPoints);
-			var angle = (theta * i);
-
-			double radiusAdaptation = Math.Cos(heightSeparatorAngle);
-			array[0] = ((sessionData.Radius * radiusAdaptation) * Math.Cos(angle));
-			array[1] = ((sessionData.Radius * radiusAdaptation) * Math.Sin(angle));
-
-			return array;
-		}
-		/// <summary>
-		/// array of distances between height centers
-		/// </summary>
-		/// <param name="numberOfPicturesInCol"></param>
-		/// <returns></returns>
-		private static double[] getHeightCenters(int numberOfPicturesInCol)
-		{
-			double[] centers = new double[numberOfPicturesInCol];
-			if (numberOfPicturesInCol % 2 == 0)
+			int minI = 0;
+			double minDistance = double.MaxValue;
+			for (int i = 0; i < photoCenters.Length; i++)
 			{
-				int[] arr = Enumerable.Range(0, numberOfPicturesInCol / 2).ToArray();
-				int[] firstPart = Enumerable.Reverse(arr).ToArray();
-				int[] secondPart = Enumerable.Range(0, numberOfPicturesInCol / 2).ToArray();
-
-				for (int i = 0; i < numberOfPicturesInCol; i++)
+				if (photoCenters[i].DistanceFrom(currentRay) < minDistance)
 				{
-					if (i < numberOfPicturesInCol / 2) { centers[i] = (double)firstPart[i] + (0.5); }
-					else { centers[i] = -1 * ((double)secondPart[i - numberOfPicturesInCol / 2] + (0.5)); }
+					minI = i;
+					minDistance = photoCenters[i].DistanceFrom(currentRay);
 				}
 			}
-			else
-			{
-				int[] arr = Enumerable.Range(1, (numberOfPicturesInCol / 2)).ToArray();
-				int[] firstPart = Enumerable.Reverse(arr).ToArray();
-				int[] secondPart = Enumerable.Range(1, (numberOfPicturesInCol / 2)).ToArray();
-
-				for (int i = 0; i < numberOfPicturesInCol; i++)
-				{
-					if (i < numberOfPicturesInCol / 2) { centers[i] = (double)firstPart[i]; }
-					else if (i == numberOfPicturesInCol / 2) { centers[i] = 0; }
-					else { centers[i] = -1 * ((double)secondPart[i - 1 - numberOfPicturesInCol / 2]); }
-				}
-			}
-			return centers;
+			return minI;
 		}
 	}
 }
